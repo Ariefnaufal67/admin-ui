3571d2e4878584a3010d8d88f9ac4ca8010a2559
ariefnaufal67 <azminaufalarief54@gmail.com>
2026-07-07 12:19:26 +0700

    pesan commit

--- admin-ui/src/Pages/Signin.jsx
+++ admin-ui/src/Pages/Signin.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import AuthLayout from '../components/Layouts/AuthLayout';
 import FormSignin from '../components/Fragments/FormSignin';
+import { loginService } from '../services/authService';
+import { AuthContext } from '../context/authContext';
 
 function Signin() {
-    return (
-        <AuthLayout title="Sign in to your account" type="login">
-            <FormSignin />
-        </AuthLayout>
-    )
+  const { login } = useContext(AuthContext);
+
+  const handleLogin = async (email, password) => {
+    try {
+      const { refreshToken } = await loginService(email, password);
+      login(refreshToken); 
+    } catch (err) {
+      console.error(err.msg);
+    }
+  };
+
+  return (
+    <AuthLayout title="Sign in to your account" type="login">
+      <FormSignin onSubmit={handleLogin} />
+    </AuthLayout>
+  )
 }
 
 export default Signin
